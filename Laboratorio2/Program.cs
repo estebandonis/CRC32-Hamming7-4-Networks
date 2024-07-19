20a3d445476fcca7f3389b799cfe0cbb9e5bf932
estebandonis <estebandonis10@gmail.com>
2024-07-18 20:19:21 -0600

    Primer Commit C#

---
+++ Laboratorio2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = Console.ReadLine() ?? "Unknown";
+        Console.WriteLine($"Hello, {name}!");
+    }
+}
