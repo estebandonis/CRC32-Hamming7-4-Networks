dea488742f4f8101641830485c76e5069354f110
estebandonis <estebandonis10@gmail.com>
2024-07-18 20:41:28 -0600

    Cambio de CRCAlgorithm

---
+++ CRCAlgorithm/Emisor/emisor.py
@@ -0,0 +1,5 @@
+def main():
+    print("Hola, soy el emisor")
+
+if __name__ == "__main__":
+    main()
---
+++ CRCAlgorithm/Receptor/Receptor/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        string message = Console.ReadLine() ?? "Default message";
+        Console.WriteLine($"Hola {message}!");
+    }
+}
